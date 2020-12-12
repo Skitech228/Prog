8fc57188efcd3133014e522622ae25c0f2eff12e
1183764 <1183764@K408-10>
2020-12-12 12:44:10 +0300

    FoolProj

--- ConsoleApp37/CYK.cs
+++ ConsoleApp37/CYK.cs
@@ -4,6 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//https://www.xarg.org/tools/cyk-algorithm/
+//S -> AB
+//A -> CD | CF
+//B -> c | EB
+//C -> a
+//D -> b
+//E -> c
+//F -> AD
+
 namespace ConsoleApp37
 {
     class CYK
@@ -15,7 +24,23 @@ namespace ConsoleApp37
             {
                 table[i] = new string[Str.Length - i];
             }
-            int count=0;
+            int count = 0;
+            int jj = 0;
+            Algoritm(Str, table, ref count, ref jj);
+            for (int i = 0; i < Str.Length; i++)
+            {
+                for (int j = 0; j < table[i].Length; j++)
+                {
+                    Console.Write(table[i][j] + "\t");
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            return table;
+        }
+
+        private static void Algoritm(string Str, string[][] table, ref int count, ref int jj)
+        {
             for (int i = 0; i < Str.Length; i++)
             {
                 for (int j = 0; j < table[i].Length; j++)
@@ -31,28 +56,24 @@ namespace ConsoleApp37
                     }
                     else
                     {
-                        count = i-1;
+                        count = i - 1;
+                        jj = j + 1;
                         for (int t = 0; t < i; t++)
                         {
-                            if (table[count][t + 1] == "C" && table[t][j] == "D" || table[count][t + 1] == "D" && table[t][j] == "C")
+                            if (table[count][jj] == "C" && table[t][j] == "D" || table[count][jj] == "D" && table[t][j] == "C" || table[count][jj] == "F" && table[t][j] == "C" || table[count][jj] == "C" && table[t][j] == "F")
                                 table[i][j] = "A";
-                            else
-                                table[i][j] = "0";
+                            if (table[count][jj] == "A" && table[t][j] == "D" || table[count][jj] == "D" && table[t][j] == "A")
+                                table[i][j] = "F";
+                            if (table[count][jj] == "B,E" && table[t][j] == "B,E")
+                                table[i][j] = "B";
+                            if (table[count][jj] == "B,E" && table[t][j] == "A" || table[count][jj] == "A" && table[t][j] == "B,E" || table[count][jj] == "B" && table[t][j] == "A" || table[count][jj] == "A" && table[t][j] == "B")
+                                table[i][j] = "S";
                             count--;
+                            jj++;
                         }
                     }
                 }
             }
-            for (int i = 0; i < Str.Length; i++)
-            {
-                for (int j = 0; j < table[i].Length; j++)
-                {
-                    Console.Write(table[i][j]+"\t");
-                }
-                Console.WriteLine();
-                Console.WriteLine();
-            }
-            return table;
         }
     }
 }
